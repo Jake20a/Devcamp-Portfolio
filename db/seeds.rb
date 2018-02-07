a85217ce45d379c1d311d81e1311054b27485f60
jake20a <jakedevcamp@gmail.com>
2018-02-07 15:53:37 -0700

    updated files upload forms and refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,69 @@
-3.times do |topic|
-  Topic.create!(
-    title: "Topic #{topic}"
-  )
+1.times do |user|    
+	User.create(
+                name: "Jake Andersen",
+                email: "jakeandersen@live.com",
+                password: "asdfasdf",
+                password_confirmation: "asdfasdf",
+                role: "site_admin"
+      )
 end
+
+
+3.times do |topic|   
+	Topic.create!(     
+		title: "Topic #{topic}"   ) 
+end 
 puts "3 Topics created"
 
 
 
-10.times do |blog|
-	Blog.create!(
-		title: "My Blog Post #{blog}",
-		body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.",
-		topic_id: Topic.last.id
-		)
+	10.times do |blog|     Blog.create!(         title: "My Blog Post #{blog}",
+		body: "It is a long established fact that a reader will be distracted by the
+		readable content of a page when looking at its layout. The point of using
+		Lorem Ipsum is that it has a more-or- less normal distribution of letters, as
+		opposed to using 'Content here, content here', making it look like readable
+		English. Many desktop publishing packages and web page editors now use Lorem
+		Ipsum as their default model text, and a search for 'lorem ipsum' will uncover
+		many web sites still in their infancy. Various versions have evolved over the
+		years, sometimes by accident, sometimes on purpose injected humour and the
+		like.",         topic_id: Topic.last.id )
 
 end
 
 puts "10 blog post created"
 
-5.times do |skill|
-	Skill.create!(
-		title:"Rails #{skill}",
-		percent_utilized: 15
-		)
-end
+5.times do |skill|     Skill.create!(         title:"Rails #{skill}",
+	percent_utilized: 15         ) end
 
 puts "5 skils created"
 
-8.times do |portfolio_item|
-	Portfolio.create!(
-		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "Ruby on Rails",
-		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures",
-		main_image: "http://via.placeholder.com/600x400",
-		thumb_image: "http://via.placeholder.com/350x200"
-		)
-end
+8.times do |portfolio_item|     Portfolio.create!(         title: "Portfolio
+	title: #{portfolio_item}",         subtitle: "Ruby on Rails",         body:
+	"There are many variations of passages of Lorem Ipsum available, but the
+	majority have suffered alteration in some form, by injected humour, or
+	randomised words which don't look even slightly believable. If you are going
+	to use a passage of Lorem Ipsum, you need to be sure there isn't anything
+	embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
+	the Internet tend to repeat predefined chunks as necessary, making this the
+	first true generator on the Internet. It uses a dictionary of over 200 Latin
+	words, combined with a handful of model sentence structures", main_image:
+	"http://via.placeholder.com/600x400",         thumb_image:
+	"http://via.placeholder.com/350x200"         ) end
 
-1.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
-  )
-end
+1.times do |portfolio_item|   Portfolio.create!(     title: "Portfolio title:
+	#{portfolio_item}",     subtitle: "Angular",     body: "Lorem ipsum dolor sit
+	amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+		et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+		ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+		in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+		officia deserunt mollit anim id est laborum.",     main_image:
+		"http://placehold.it/600x400",     thumb_image: "http://placehold.it/350x200"
+		) 
+end 
 puts "9 portfolio iteams created"
 
-3.times do |technology|
-   Portfolio.last.technologies.create!(
-    name: "Technology #{technology}",
-  )
-end
+3.times do |technology|    Portfolio.last.technologies.create!( name:
+	"Technology #{technology}",   ) end
 
 puts "3 Technologies created"
