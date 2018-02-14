9fa054fa7b183234c841ce496e69bb2e8412aa92
jake20a <jakedevcamp@gmail.com>
2018-02-14 14:08:59 -0700

    built out topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
+  resources :topics, only: [:index, :show]
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -11,13 +12,12 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
 
-
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-  
+
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
